e3d064228cd4031635dddae3be7bf28ca1c71770
Deepak <53943221+Deepakmoon143@users.noreply.github.com>
2023-08-13 00:54:07 +0530

    Question_3.py

--- Question_3.py
+++ Question_3.py
@@ -1,10 +1,10 @@
 user = int(input("enter the value"))
-even_num = 0
-odd_num = 0
+even_number = 0
+odd_number = 0
 for i in range(1,user+1):
     if i %2==0:
-        even_num+=1
+        even_number+=1
     else:
-        odd_num+=1
-print("number of even_numbers: ",even_num)
-print("number of odd_numbers: ",odd_num)
+        odd_number+=1
+print("number of even_numbers: ",even_number)
+print("number of odd_numbers: ",odd_number)
